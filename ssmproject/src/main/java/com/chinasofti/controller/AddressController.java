9382b749de10030d3c6738b5b34dbd7af42baa60
zhenghongxu1 <710096382@qq.com>
2019-09-19 09:52:04 +0800

    first commit

---
+++ ssmproject/src/main/java/com/chinasofti/controller/AddressController.java
@@ -0,0 +1,39 @@
+package com.chinasofti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.chinasofti.mapper.AjiaShippingMapper;
+import com.chinasofti.pojo.AjiaShipping;
+
+@Controller
+@RequestMapping("/address")
+public class AddressController {
+	
+	@Autowired
+	private  AjiaShippingMapper mapper;
+	
+	
+	@RequestMapping("/selectOne")
+	public String  selectOne(Model model) {
+		
+		AjiaShipping shipping = mapper.selectByPrimaryKey(1l);
+		System.out.println(shipping);
+		model.addAttribute("shipping", shipping);
+		
+		return "/index.jsp";
+		
+	}
+	@RequestMapping("/insertOne")
+	public String  insertOne(Model model) {
+		AjiaShipping ajiaShipping = new AjiaShipping();
+		ajiaShipping.setUserId(50l);
+		ajiaShipping.setStatus((byte)1);
+		ajiaShipping.setIsDefault(true);
+		int insert = mapper.insert(ajiaShipping);
+		return "/index.jsp";
+	}
+	
+}
